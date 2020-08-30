a030e95ad0d5fd3e375415cc6e696a5c7ff4dd9e
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-08-30 17:33:53 -0400

    changed some code to make it more understandable me as I though it would look be easy for me to understand

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var score = 0;
 var questionIndex = 0;
 
 var currentTime = document.querySelector("#currentTime");
-var timer = document.querySelector("#startTime");
+var timer = document.querySelector("#startGame");
 var questionsDiv = document.querySelector("#questionsDiv");
 var wrapper = document.querySelector("#wrapper");
 
